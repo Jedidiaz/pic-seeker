668cf7a853c4bb5561b5b289a584da494fa475e2
Jedidiaz <jedidiazfagundez@gmail.com>
2024-06-19 00:32:01 -0500

    PSUIKIT002 - feat: Add Search view and Actions

--- PSUIKIT/PSUIKIT/Controllers/SerachViewController.swift
+++ PSUIKIT/PSUIKIT/Controllers/SerachViewController.swift
@@ -9,10 +9,31 @@ import UIKit
 
 class SerachViewController: UIViewController {
 
+    //MARK: - Oulets
+    @IBOutlet weak var searchTextField: UITextField!
+    @IBOutlet weak var searchButton: UIButton!
+    
+    //Variables
+    var textSearch = ""
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.title = "Serach"
         // Do any additional setup after loading the view.
+        
+        searchTextField.delegate = self
     }
+    
+    //MARK: - Actions
+    @IBAction func SearchAction(_ sender: UIButton) {
+        print(textSearch)
+    }
+}
 
+//MARK: - UITextFieldDelegate
+extension SerachViewController: UITextFieldDelegate {
+    func textFieldDidChangeSelection(_ textField: UITextField) {
+        self.textSearch = textField.text?.trimmingCharacters(in: .whitespacesAndNewlines) ?? ""
+    }
 }
